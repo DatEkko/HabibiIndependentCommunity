0b4702b22a7c5383db0ee56b6e9852039b56a372
NgTienDat <ngtiendat242002@gmail.com>
2026-04-23 09:47:51 +0700

    day 2.1

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,11 +1,28 @@
 import type { Metadata } from "next";
-import { Geist, Geist_Mono } from "next/font/google";
 import "@/styles/globals.css";
 import SmoothScroll from "@/util/SmoothScroll";
 
-export const metadata: Metadata = {
-  title: "Habibi Independent Community",
-  description: "Habibi Independent Community",
+const baseUrl = 'https://habibi-independent-community.vercel.app/';
+
+export const metadata = {
+  title: 'Habibi Independent Community',
+  description: 'Cộng đồng dành cho những người yêu thích đô vật và Trung Đông',
+  openGraph: {
+    title: 'Habibi Independent Community - Gia nhập ngay',
+    description: 'Nơi kết nối đam mê và tinh thần võ sĩ Trung Đông.',
+    url: baseUrl,
+    siteName: 'Habibi Community',
+    images: [
+      {
+        url: `${baseUrl}/metadata-logo.png`, // Trỏ trực tiếp vào file trong thư mục public
+        width: 1200,
+        height: 630,
+        alt: 'Habibi Community Logo',
+      },
+    ],
+    locale: 'vi_VN',
+    type: 'website',
+  },
 };
 
 export default function RootLayout({
